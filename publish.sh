47219e7fef80cd64498d4fe5e18dca41beadf929
Scheuer Jan <scheuer@email.cz>
2017-08-03 06:24:27 +0200

    publish predelano na scp

--- publish.sh
+++ publish.sh
@@ -1,1 +1,3 @@
-cp -r /home/jan/pbx/* /run/user/1000/gvfs/sftp\:host\=192.168.2.7\,port\=222\,user\=root/var/lib/asterisk/static-http/c/
+#cp -r /home/jan/pbx/* /run/user/1000/gvfs/sftp\:host\=192.168.2.7\,port\=222\,user\=root/var/lib/asterisk/static-http/c/
+echo Zepter2x.
+scp -P 222 -r /home/jan/pbx/* root@192.168.2.7:/var/lib/asterisk/static-http/c/ 
